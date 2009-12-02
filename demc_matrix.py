4d80d5cd7a05018954ac6d3ecd79345853e79d60
Jose Gomez-Dans <jgomezdans@gmail.com>
2009-12-02 11:20:55 +0000

    Added the Wishart matrix update. Appears to work, but convergence is only tested on the non-matrix parameters.

--- demc_matrix.py
+++ demc_matrix.py
@@ -3,7 +3,7 @@ import numpy
 import numpy.linalg
 import scipy.stats
 import sys, pdb
-
+from pymc import inverse_wishart_like, rinverse_wishart
 
 def full_gauss_den(x, mu, va, log):
 	""" This function is the actual implementation
@@ -59,7 +59,7 @@ class DEMC_sampler(object):
 
 	The first call instantiates the DEMC_sampler class. It is initialised with population size (10 in this example), and with the number of generations (or iterations).  The second line defines a list of pairs of parameter names, distributions. The distributions are distribution objects from scipy.stats, or code that implements the same methods that the user provides. Note that we do not provide a reference to these objects, but write the reference as a string (the reference is obtained internally). We also provide a list with the parameter names in the same order they are required in the M{\theta} parameter vector. These two lists are then included in the object by calling the prior_distributions() method. The sampler is initialised with a M{Z} matrix (num_params x [...]), sampled from the prior distributions. Or, you can provide your own. Finally, the sampler is started and it returns the Z_out matrix (the samples for the different parameters) and the accepctance rate (try to get it ~0.2-0.4)
 	"""
-	def __init__ ( self, num_population, CR=1.0, F=2.38, pSnooker=0.1, pGamma1=0.1, n_generations=1000, n_thin=5, n_burnin=200, eps_mult=0.1, eps_add=0) :
+	def __init__ ( self, num_population, CR=1.0, F=2.38, pSnooker=0.1, pGamma1=0.01, n_generations=1000, n_thin=5, n_burnin=200, eps_mult=0.1, eps_add=0) :
 		"""
 		The class creator. You should pass it the populations (say between 10 or 100), and the sampler parameters, such as n_generations, n_thing and n_burnin. The other parameters are DEMC-related parameters and shouldn't need changing accroding to Vrugt and ter Braak. CR is a crossover rate, and at present, is not used in this implementation of the code (but could be used in a future implementation that does DREAM)
 		"""
@@ -73,6 +73,22 @@ class DEMC_sampler(object):
 		self.n_burnin =n_burnin
 		self.eps_mult = eps_mult
 		self.eps_add = eps_add
+		self.CreateObservations ()
+		
+	def CreateObservations ( self, x=3, b=0.1):
+		x=numpy.r_[-5:5:.5]
+		a=3
+		b=0.1
+		x1=a*x+b
+		x2=a*x+b
+		mu=numpy.array([0,0])
+		cov=numpy.array([[1.,0.1*numpy.sqrt(3.2)],[0.1*numpy.sqrt(3.2),3.2]])
+		d=numpy.random.multivariate_normal (mu,cov,size=x1.shape)
+		X1=x1+d[:,0] #+(numpy.random.rand(x1.shape[0])-0.5)*8#Actually x1==x2, but nevermind!
+		X2=x2+d[:,1] #+(numpy.random.rand(x1.shape[0])-0.5)*8
+		self.observations = numpy.c_[ X1, X2 ]
+		print self.observations
+		
 	def choose_without_replacement(self, m,n,repeats=None):
 		"""Choose n nonnegative integers less than m without replacement
 		Returns an array of shape n, or (n,repeats).
@@ -116,59 +132,47 @@ class DEMC_sampler(object):
 		self.parameters = parameters # List is in the order of the parameter vector
 		for [k,v] in parameter_list:
 			setattr(self, k, eval( v ))
-			
-	def prior_probabilities ( self, theta ):
+	def Wishart_Params ( self, DoF, Tau ):
+		"""
+		This method sets the parameters for the Wishart/Inverse
+		Wishart prior: the degrees of freedom (n) and the
+		(inverse) scale matrix (p). I think that n>p-1,
+		and Tau has to be positive definite.
+		"""
+		assert ( DoF>(Tau.shape[0]-1))
+		self.wishart_dof = DoF
+		self.Tau = Tau
+		
+	def prior_probabilities ( self, theta, W ):
 		""" The method that calculates the prior (log) probabilities. This is based on the prior distributions given in prior_distributions, and assumes independence, so we just add them up. 
 		"""
 		p = numpy.array([ numpy.log ( getattr ( self, self.parameters[i]).pdf ( theta[i])) for i in xrange(len(self.parameters)) ]).sum()
+		p += inverse_wishart_like ( W, self.wishart_dof, self.Tau )
+		#if numpy.isneginf(p):
+			#p = numpy.log(1.0E-300)
+		if p<1.0e-100:
+			p = -numpy.infty
+			
+		return p
+
+	def likelihood_function ( self, theta, W):
+
+		x = numpy.r_[-5:5:.5]
+		fwd = theta[0]*x + theta[1]
+		D = fwd[:,numpy.newaxis]-self.observations
+		
+		p = - numpy.log(2.*numpy.pi)  - numpy.log(numpy.sqrt ( numpy.linalg.det (W))) -\
+				0.5*numpy.array([ numpy.dot(numpy.dot(D[i,:],numpy.linalg.inv(W)),
+				D[i,:]) for i in xrange(D.shape[0])]).sum()
 		if numpy.isneginf(p):
 			p = numpy.log(1.0E-300)
 		return p
 
-	#def likelihood_function ( self, theta ):
-		#"""For example! This function ought to be overridden by the user, and maybe extended with whatever extra parameters you need to get hold of your observations, or model driver parameters.
-		#This function method calculates the likelihood function for a vector M{\theta}. Usually, you have a model you run with these parameters as inputs (+ some driver data), and some observations that go with the output of the forward model output. These two sets of values are combined in some sort of cost function/likelihood function. A common criterion is to assume that the model is able to perfectly replicate the observations (given a proper parametrisation). The only mismatch between model output and observations is then given by the uncertainty with which the measurement is performed, and we can encode this as a zero-mean Normal distribution. The variance of this distribution is then related to the observational error. If different measurements are used, a multivariate normal is useful, and correlation between observations can also be included, if needs be.
-		#"""
-		#means = numpy.matrix([-3.0, 2.8])
-		#means = numpy.matrix([-5.0, 5])
-		#sigma1 = 1.0
-		#sigma2 = 2#0.5
-		#rho = -0.5#-0.1
-		#covar = numpy.matrix([[sigma1*sigma1,rho*sigma1*sigma2],[rho*sigma1*sigma2,sigma2*sigma2]])
-		#inv_covar = numpy.linalg.inv ( covar ) # numpy.matrix([[  5.26315789,   9.47368421],\
-						##[  9.47368421,  21.05263158]])
-		#det_covar = numpy.linalg.det( covar ) #0.047499999999999987
-		#N = means.shape[0]
-		#X = numpy.matrix(means- theta)
-		##X = theta
-		##p = full_gauss_den(X, means, covar, True)
-		##This is just lazy... Using libraries to invert a 2x2 matrix & calc. its determinant....
-		##Also, the log calculations could be done more efficiently and stored, but...
-		#p = pow(1.0/(2*numpy.pi), N/2.)
-		#p = p / numpy.sqrt ( numpy.linalg.det (covar))
-		##p = 0.73025296137109341 # Precalc'ed
-		##p = p *	numpy.exp (-0.5*X*inv_covar*X.transpose())
-		#a = X*inv_covar*X.T
-		#p = p*numpy.exp(-0.5*a)
-		##pdb.set_trace()
-		#p = numpy.log(p)
-		#if numpy.isneginf(p):
-			#p = numpy.log(1.0E-300)
-		#return p
-		#####x1 = theta[0] ; x2 = theta[1]
-		####sigma1=1.0
-		####sigma2=2.0
-		####mu1=-5.0
-		####mu2=5.0
-		####ret=1.0/numpy.sqrt(2*numpy.pi*sigma1**2) * numpy.exp(-1/(2*sigma1**2) * (mu1-x1)**2)\
-		####*1/numpy.sqrt(2*numpy.pi*sigma2**2) * numpy.exp(-1/(2*sigma2**2) * (mu2-x2)**2) ;
-		####return numpy.log(ret)
-
-	def fitness ( self, theta ):
+	def fitness ( self, theta, W ):
 		"""
 		The new posterior probability in log. Convenience, really
 		"""
-		return self.likelihood_function ( theta ) + self.prior_probabilities ( theta )
+		return self.likelihood_function ( theta,W ) + self.prior_probabilities ( theta, W )
 		
 	###def MonitorChains ( self, x ):
 		###alpha = 0.05                     # 95% intervals
@@ -221,8 +225,10 @@ class DEMC_sampler(object):
 			#pdb.set_trace()
 			if rhat<numpy.sqrt(V_hat/W):
 				rhat = numpy.sqrt(V_hat/W)
-			
-		
+			print numpy.sqrt(V_hat/W)
+			print [scipy.stats.scoreatpercentile ( x.flatten(1), percentile) for percentile in [2.5, 25., 50., 75., 97.5] ]
+			print x.flatten(1).mean(), x.flatten(1).std()
+		print "---------------------------------------------------------"
 		return rhat
 			
 			
@@ -239,29 +245,38 @@ class DEMC_sampler(object):
 		Z = []
 		for i in xrange(len(self.parameters)):
 			Z.append(getattr ( self, self.parameters[i]).rvs ( size = m0))
-		return numpy.array(Z)
+		Zw = [ rinverse_wishart ( self.wishart_dof, self. Tau) for i in xrange(m0)]
+		Zw = numpy.array(Zw)
+		Zw=numpy.rollaxis(numpy.rollaxis(Zw,1),-1)
+		return numpy.array(Z), Zw
 
-	def demc_zs ( self,  Z):
+	def demc_zs ( self,  Z, Zw):
 		#CR=self.CR, F=self.F, pSnooker=0.1, pGamma1=0.1, n_generations=10000, n_thin=5, n_burnin=2000, eps_mult=0.1, eps_add=0
 		#Z = numpy.zeros ( d, m0)
 		#X = numpy.zeros (d, num_population)
-
+		#pSnooker=0.
 		d = 2
 		X = Z[:,:self.num_population]
+		Xw = Zw[:,:,:self.num_population]
 		m0 = Z.shape[1]
 		self.discard = int(m0+self.num_population*numpy.floor(self.n_burnin/self.n_thin))
 		mZ = Z.shape[1]
 		Npar = X.shape[0]
+		Npar = 2
 		Npar12 = (Npar-1)/2. # Factor for Metropolis ratio DE snooker update
+		Npar12 = 2.
 		F2 = self.F/numpy.sqrt ( 2.*Npar)
+		F2 = self.F/numpy.sqrt(10.)
 		F1 = 1.0
 		accept = numpy.zeros ( self.n_generations )
 		#iseq = numpy.arange(1, num_population)
-		rr = 0.0 ; r_extra = 0
+		rr = 0.0 ; r_extra = 0#numpy.log(0.)
 		#print int(m0+num_population*numpy.floor(n_burnin/n_thin))
 		#Calculate the starting posteriors...
 		Z_diagnostic = numpy.zeros ( (Npar, self.num_population, self.n_generations))
-		logfitness_x = [ self.fitness(X[:,i]) for i in xrange(self.num_population) ]
+
+		logfitness_x = [ self.fitness(X[:,i], Xw[:,:,i]) for i in xrange(self.num_population) ]
+
 		#Start of main loop
 		iteration = -1
 		while True:
@@ -270,47 +285,68 @@ class DEMC_sampler(object):
 			for i in xrange(self.num_population):
 				#Start of different chains loop
 				#First decide whether this is a snooker update or not
+				#pdb.set_trace()
 				if (numpy.random.random()<self.pSnooker):
 					#Snooker update
 					#Select three chains
-					rr = self.choose_without_replacement(mZ-1, 3, repeats=None)
+					while True:
+						rr = self.choose_without_replacement(mZ, 3, repeats=None)
+						rr-=1
+						if not (numpy.any(rr==i)): break
 					z = Z[:,rr[2]]
+					zw = Zw[:,:,rr[2]]
 					x_z = X[:,i] - z
+					x_zw =Xw[:,:,i] - zw
 					#Difference between the current point and one of the 3 chains
 					D2 = max(numpy.sum(x_z*x_z), 1.0e-300) # This is the distance. Could do it with dot?
-					gamma_snooker = numpy.random.random()+1.2 # Snooker stochastic
+					D2w = max(numpy.sum(x_zw*x_zw), 1.0e-300)
+					gamma_snooker = numpy.random.random()+0.2 # Snooker stochastic
 					proj_diff = numpy.sum((Z[:,rr[0]] - Z[:,rr[1]])*x_z)/D2 # Project the difference onto x_z. Normalize by x_z's norm
+					proj_diff_w = numpy.sum((Zw[:,:,rr[0]] - Zw[:,:,rr[1]])*x_zw)/D2w # Project the difference onto
 					x_prop = X[:,i] + (gamma_snooker * proj_diff) * x_z # Proposed point
+					xw_prop = Xw[:,:,i] + (gamma_snooker * proj_diff_w) * x_zw # Proposed matrix
 					x_z = x_prop - z # update x_z
+					x_zw = xw_prop - zw # update x_z
 					#pdb.set_trace()
 					D2prop = max( numpy.dot(x_z, x_z), 1.0e-30) # Calculate D2prop
+#					D2propw = max( numpy.dot(x_zw, x_zw), 1.0e-30) # Calculate D2prop
 					r_extra = Npar12*(numpy.log(D2prop) - numpy.log(D2))
+					
 				else:
 					if (numpy.random.random()<self.pGamma1):
 						gamma_par = F1
 					else:
 						gamma_par = F2 * numpy.random.uniform( low=1-self.eps_mult, high=1+self.eps_mult, size=Npar)
-					rr = self.choose_without_replacement(mZ-1, 2, repeats=None)
+					
+					while True:
+						rr = self.choose_without_replacement(mZ, 2, repeats=None)-1
+						if not ( numpy.any(rr==i)): break
+					#pdb.set_trace()
 					if (self.eps_add==0):
 						x_prop = X[:,i] + gamma_par * ( Z[:,rr[0]] - Z[:,rr[1]])
+						xw_prop = Xw[:,:,i] + gamma_par * ( Zw[:,:,rr[0]] - Zw[:,:,rr[1]])
 					else:
 						x_prop = X[:,i] + gamma_par * ( Z[:,rr[0]] - Z[:,rr[1]]) + self.eps_add*numpy.random.randn(Npar)
-					r_extra = 0
-				logfitness_x_prop = self.fitness ( x_prop )
+						
+						xw_prop = Xw[:,:,i] + gamma_par * ( Zw[:,:,rr[0]] - Zw[:,:,rr[1]]) + self.eps_add*numpy.tril(numpy.random.randn(self.Tau.flatten().shape[0]).reshape(self.Tau.shape))
+					#r_extra = 0
+				#W_test=numpy.array([[1,-0.8*numpy.sqrt(3.2)],[-0.8*numpy.sqrt(3.2),3.2]])
+				logfitness_x_prop = self.fitness ( x_prop, xw_prop )
 				logr = logfitness_x_prop - logfitness_x[i]
-				print i
 				if (logr + r_extra)>numpy.log ( numpy.random.random()):
 					accepti += 1
 					X[:,i] = x_prop
+					Xw[:,:,i] = xw_prop
 					logfitness_x[i] = logfitness_x_prop
 				Z_diagnostic [:, i, iteration] = X[:,i]
 			accept[iteration] = accepti
 			iteration += 1
 			#print logfitness_x[i], logfitness_x_prop ,logr,r_extra,accepti,x_prop
-			print iteration, accepti
+			#print iteration, accepti,X.mean(axis=1),Xw.mean(axis=2)
 			
 			if iteration%self.n_thin==0:
 				Z = numpy.c_[X,Z]
+				Zw = numpy.c_[Xw,Zw]
 				mZ = Z.shape[1]
 				#print "iteration ",iteration
 				#print "\t",X[0,:]
@@ -320,21 +356,19 @@ class DEMC_sampler(object):
 				print iteration, rhat
 				new_pop = iteration
 				break
-				if rhat<=1.102: break
+				if rhat<=1.02: break
 				accept = numpy.append ( accept, numpy.zeros (self.n_generations+1), 0)
 				Z_diagnostic = numpy.append(Z_diagnostic, numpy.zeros ((Npar, self.num_population, self.n_generations+1)), 2)
-				
-		return (Z[:,:int(m0+iteration*numpy.floor(self.n_burnin/self.n_thin))], accept/self.num_population)
+		return (Z, Zw, accept/self.num_population)
+		#return (Z[:,:int(m0+iteration*numpy.floor(self.n_burnin/self.n_thin))], accept/self.num_population)
 
 if __name__=="__main__":
-	DEMC = DEMC_sampler ( 10, n_generations=200, n_burnin=1, n_thin=1)
-	parameter_list=[['x1', 'scipy.stats.norm(-100, 20)'], ['x2', 'scipy.stats.norm(-100, 20)']]
+	DEMC = DEMC_sampler ( 4, n_generations=50000, n_burnin=1, n_thin=1)
+	parameter_list=[['x1', 'scipy.stats.norm(1, 2)'], ['x2', 'scipy.stats.norm(0, 2)']]
 	parameters = ['x1','x2']
 	DEMC.prior_distributions ( parameter_list, parameters )
-	Z = DEMC.ProposeStartingMatrix ( 150 )
-	(Z_out, accept_rate) = DEMC.demc_zs ( Z )
-	import pylab
-	pylab.figure();pylab.plot ( Z_out[0,:], Z_out[1,:], 'k,')
-	pylab.figure();pylab.hist(Z_out[0,:],bins=10);pylab.hist(Z_out[1,:],bins=10);
-	pylab.figure();pylab.hexbin(Z_out[0,:], Z_out[1,:], bins='log')
-	pylab.show()
+	cov=numpy.array([[1,-0.8*numpy.sqrt(3.2)],[-0.8*numpy.sqrt(3.2),3.2]])
+	cov = numpy.array([[1.,0.],[0,1]])
+	DEMC.Wishart_Params ( 2, cov)
+	(Z, Zw) = DEMC.ProposeStartingMatrix ( 10 )
+	(Z_out, Zw_out, accept_rate) = DEMC.demc_zs ( Z, Zw )
